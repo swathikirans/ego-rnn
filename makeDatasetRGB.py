ef41400e27120694ab4ca2bfc811c6f464f2d673
rm-wu <riccardomereu5@gmail.com>
2020-10-12 18:56:18 +0200

    fixed error in makeDatasetRGB

--- makeDatasetRGB.py
+++ makeDatasetRGB.py
@@ -15,11 +15,11 @@ def gen_split(root_dir, splits, stack_size):
     # root_dir  =  #drive/.../GTEA61/processed_frames/
     for split in splits:  # root_dir/SX
         dir1 = os.path.join(root_dir, split)
-        print(dir1)
+        #print(dir1)
         class_id = 0
         for target in sorted(os.listdir(dir1)):  # root_dir/SX/target/
             dir2 = os.path.join(dir1, target)
-            print(dir2)
+            #print(dir2)
             insts = sorted(os.listdir(dir2))  #  root_dir/SX/target/Y
             if insts != []:
                 for inst in insts:
@@ -27,8 +27,8 @@ def gen_split(root_dir, splits, stack_size):
                     numFrames = len(glob.glob1(os.path.join(inst_dir, 'rgb'), '*[0-9].png'))
                     if numFrames >= stack_size:
                         Dataset.append(inst_dir)
-                    Labels.append(class_id)
-                    NumFrames.append(numFrames)
+                        Labels.append(class_id)
+                        NumFrames.append(numFrames)
 
             class_id += 1
     return Dataset, Labels, NumFrames
@@ -40,12 +40,14 @@ class makeDataset(Dataset):
                  stack_size=5,
                  seqLen=20,
                  train=True,
-                 fmt='.png'):
+                 fmt='.png',
+                 verbose = False):
         self.images, self.labels, self.numFrames = gen_split(root_dir, splits, stack_size)
         self.spatial_transform = spatial_transform
         self.train = train
         self.seqLen = seqLen
         self.fmt = fmt
+        self.verbose = verbose
 
     def __len__(self):
         return len(self.images)
@@ -56,7 +58,8 @@ class makeDataset(Dataset):
         numFrame = self.numFrames[idx]
         inpSeq = []
         self.spatial_transform.randomize_parameters()
-
+        if self.verbose:
+            print(vid_name, idx)
         for i in np.linspace(1, numFrame, self.seqLen, endpoint=False):
             fl_name = vid_name + '/rgb/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
             img = Image.open(fl_name)
