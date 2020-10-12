265eaf473c068d3d50a62e1fd8903ddb789b1349
rm-wu <riccardomereu5@gmail.com>
2020-10-12 16:45:21 +0200

    Updated the RGB dataset to import images from GDrive

--- makeDatasetRGB.py
+++ makeDatasetRGB.py
@@ -7,37 +7,42 @@ import glob
 import random
 
 
-def gen_split(root_dir, stackSize):
+def gen_split(root_dir, splits, stack_size):
     Dataset = []
     Labels = []
     NumFrames = []
-    root_dir = os.path.join(root_dir, 'frames')
-    for dir_user in sorted(os.listdir(root_dir)):
+
+    # root_dir  =  #drive/.../GTEA61/processed_frames/
+    for split in splits:  # root_dir/SX
+        dir1 = os.path.join(root_dir, split)
+        # print(dir1)
         class_id = 0
-        dir = os.path.join(root_dir, dir_user)
-        for target in sorted(os.listdir(dir)):
-            dir1 = os.path.join(dir, target)
-            insts = sorted(os.listdir(dir1))
+        for target in sorted(os.listdir(dir1)):  # root_dir/SX/target/
+            dir2 = os.path.join(dir1, target)
+            insts = sorted(os.listdir(dir2))  #  root_dir/SX/target/Y
             if insts != []:
                 for inst in insts:
-                    inst_dir = os.path.join(dir1, inst)
-                    numFrames = len(glob.glob1(inst_dir, '*.jpg'))
-                    if numFrames >= stackSize:
+                    inst_dir = os.path.join(dir2, inst)
+                    numFrames = len(glob.glob1(os.path.join(inst_dir, 'rgb'), '*[0-9].png')
+                    if numFrames >= stack_size:
                         Dataset.append(inst_dir)
-                        Labels.append(class_id)
-                        NumFrames.append(numFrames)
-            class_id += 1
+                    Labels.append(class_id)
+                    NumFrames.append(numFrames)
+
+                    class_id += 1
     return Dataset, Labels, NumFrames
 
-class makeDataset(Dataset):
-    def __init__(self, root_dir, spatial_transform=None, seqLen=20,
-                 train=True, mulSeg=False, numSeg=1, fmt='.jpg'):
 
-        self.images, self.labels, self.numFrames = gen_split(root_dir, 5)
+class makeDataset(Dataset):
+    def __init__(self, root_dir, splits,
+                 spatial_transform=None,
+                 stack_size=5,
+                 seqLen=20,
+                 train=True,
+                 fmt='.png'):
+        self.images, self.labels, self.numFrames = gen_split(root_dir, splits, stack_size)
         self.spatial_transform = spatial_transform
         self.train = train
-        self.mulSeg = mulSeg
-        self.numSeg = numSeg
         self.seqLen = seqLen
         self.fmt = fmt
 
@@ -50,8 +55,9 @@ class makeDataset(Dataset):
         numFrame = self.numFrames[idx]
         inpSeq = []
         self.spatial_transform.randomize_parameters()
+
         for i in np.linspace(1, numFrame, self.seqLen, endpoint=False):
-            fl_name = vid_name + '/' + 'image_' + str(int(np.floor(i))).zfill(5) + self.fmt
+            fl_name = vid_name + '/rgb/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
             img = Image.open(fl_name)
             inpSeq.append(self.spatial_transform(img.convert('RGB')))
         inpSeq = torch.stack(inpSeq, 0)
