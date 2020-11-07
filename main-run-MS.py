351057528f0db9e409ca287fd7195e3a4a92d2de
rm-wu <riccardomereu5@gmail.com>
2020-11-07 06:15:53 +0100

    MS with trainig two stages

--- main-run-MS.py
+++ main-run-MS.py
@@ -83,51 +83,61 @@ def main_run(dataset, stage, train_data_dir, val_data_dir, stage1_dict, out_dir,
     trainInstances = vid_seq_train.__len__()
 
     train_params = []
+    if stage == 1:
 
-    model = SelfSupervisedAttentionModel(num_classes=num_classes, mem_size=memSize)
+        model = SelfSupervisedAttentionModel(num_classes=num_classes, mem_size=memSize)
+        model.train(False)
+        for params in model.parameters():
+            params.requires_grad = False
+    else:
 
-    model.train(False)
-    for params in model.parameters():
-        params.requires_grad = False
-    # Add all the parameters of the model
-    # ResNet feature extractor
-    for params in model.resNet.layer4[0].conv1.parameters():
-        params.requires_grad = True
-        train_params += [params]
+        model = SelfSupervisedAttentionModel(num_classes=num_classes, mem_size=memSize)
+        model.load_state_dict(torch.load(stage1_dict))
+        model.train(False)
+        for params in model.parameters():
+            params.requires_grad = False
+        #
+        for params in model.resNet.layer4[0].conv1.parameters():
+            params.requires_grad = True
+            train_params += [params]
 
-    for params in model.resNet.layer4[0].conv2.parameters():
-        params.requires_grad = True
-        train_params += [params]
+        for params in model.resNet.layer4[0].conv2.parameters():
+            params.requires_grad = True
+            train_params += [params]
 
-    for params in model.resNet.layer4[1].conv1.parameters():
-        params.requires_grad = True
-        train_params += [params]
+        for params in model.resNet.layer4[1].conv1.parameters():
+            params.requires_grad = True
+            train_params += [params]
 
-    for params in model.resNet.layer4[1].conv2.parameters():
-        params.requires_grad = True
-        train_params += [params]
+        for params in model.resNet.layer4[1].conv2.parameters():
+            params.requires_grad = True
+            train_params += [params]
 
-    for params in model.resNet.layer4[2].conv1.parameters():
-        params.requires_grad = True
-        train_params += [params]
-    #
-    for params in model.resNet.layer4[2].conv2.parameters():
-        params.requires_grad = True
-        train_params += [params]
-    #
-    for params in model.resNet.fc.parameters():
-        params.requires_grad = True
-        train_params += [params]
+        for params in model.resNet.layer4[2].conv1.parameters():
+            params.requires_grad = True
+            train_params += [params]
+        #
+        for params in model.resNet.layer4[2].conv2.parameters():
+            params.requires_grad = True
+            train_params += [params]
+        #
+        for params in model.resNet.fc.parameters():
+            params.requires_grad = True
+            train_params += [params]
 
-    model.resNet.layer4[0].conv1.train(True)
-    model.resNet.layer4[0].conv2.train(True)
-    model.resNet.layer4[1].conv1.train(True)
-    model.resNet.layer4[1].conv2.train(True)
-    model.resNet.layer4[2].conv1.train(True)
-    model.resNet.layer4[2].conv2.train(True)
-    model.resNet.fc.train(True)
+        model.resNet.layer4[0].conv1.train(True)
+        model.resNet.layer4[0].conv2.train(True)
+        model.resNet.layer4[1].conv1.train(True)
+        model.resNet.layer4[1].conv2.train(True)
+        model.resNet.layer4[2].conv1.train(True)
+        model.resNet.layer4[2].conv2.train(True)
+        model.resNet.fc.train(True)
 
-    # ConvLSTM parameters
+        # Add params from ms_module
+        for params in model.ms_module.parameters():
+            params.requires_grad = True
+            train_params += [params]
+
     for params in model.lstm_cell.parameters():
         params.requires_grad = True
         train_params += [params]
@@ -160,16 +170,20 @@ def main_run(dataset, stage, train_data_dir, val_data_dir, stage1_dict, out_dir,
         iterPerEpoch = 0
         writer.add_scalar('lr', optimizer_fn.param_groups[0]['lr'], epoch + 1)
 
-        model.train(True)
-        # model.lstm_cell.train(True)
-        # model.classifier.train(True)
-        # model.resNet.layer4[0].conv1.train(True)
-        # model.resNet.layer4[0].conv2.train(True)
-        # model.resNet.layer4[1].conv1.train(True)
-        # model.resNet.layer4[1].conv2.train(True)
-        # model.resNet.layer4[2].conv1.train(True)
-        # model.resNet.layer4[2].conv2.train(True)
-        # model.resNet.fc.train(True)
+        #model.train(True)
+        model.lstm_cell.train(True)
+        model.classifier.train(True)
+        if stage == 2:
+            model.resNet.layer4[0].conv1.train(True)
+            model.resNet.layer4[0].conv2.train(True)
+            model.resNet.layer4[1].conv1.train(True)
+            model.resNet.layer4[1].conv2.train(True)
+            model.resNet.layer4[2].conv1.train(True)
+            model.resNet.layer4[2].conv2.train(True)
+            model.resNet.fc.train(True)
+
+            model.ms_module.train(True)
+
         for i, (inputsRGB, inputsMS, targets) in enumerate(train_loader):
             # Inputs:
             #   - inputsRGB : the rgb frame input
